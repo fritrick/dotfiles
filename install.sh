9457d8dd297137c807cba7fedc3424796c4b5fc2
fritrick <alexander.komogorov@gmail.com>
2020-05-03 17:45:51 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/tomasr/molokai.git && mkdir -p ~/.vim/colors && cp molokai/colors/molokai.vim ~/.vim/colors && rm -rf molokai
+
+brew install zsh
